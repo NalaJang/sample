2194f2ab4a0538de4d97cf9fc2d224a79ae24515
nala0897@naver.com <nala0897@naver.com>
2024-05-15 22:48:15 +0900

    feat: 태그 UI 구현

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:search_ex/design/color_style.dart';
 import 'package:search_ex/presentation/search_screen.dart';
 import 'package:search_ex/presentation/viewModel/home_view_model.dart';
 
@@ -11,6 +12,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int selectedTag = -1;
+
   @override
   void initState() {
     super.initState();
@@ -53,13 +56,72 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
           ),
-        HomeScreenStatus.success => ListView.builder(
-            itemCount: viewModel.savedFoodList.length,
-            itemBuilder: (context, index) => ListTile(
-              title: Text(viewModel.savedFoodList[index].foodName),
-            ),
+        HomeScreenStatus.success => Column(
+            children: [
+              _tags(),
+              const SizedBox(height: 12),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: viewModel.savedFoodList.length,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text(viewModel.savedFoodList[index].foodName),
+                  ),
+                ),
+              ),
+            ],
           ),
       },
+    );
+  }
+
+  Widget _tags() {
+    return SizedBox(
+      height: 35,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        children: [
+          const Text('전체'),
+          const Text('탄수화물'),
+          const Text('단백질'),
+          const Text('지방'),
+        ].asMap().entries.map(
+          (entry) {
+            final index = entry.key;
+            final item = entry.value;
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedTag = index;
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(7, 5, 7, 5),
+                margin: const EdgeInsets.symmetric(horizontal: 7),
+                decoration: BoxDecoration(
+                    color: selectedTag == index
+                        ? AppColors.purple2
+                        : AppColors.white,
+                    border: Border.all(
+                      color: selectedTag == index
+                          ? AppColors.purple2
+                          : AppColors.black,
+                    ),
+                    borderRadius: BorderRadius.circular(12),),
+                child: Text(
+                  item.data!,
+                  style: selectedTag == index
+                      ? const TextStyle(
+                          color: AppColors.white,
+                          fontWeight: FontWeight.bold,
+                        )
+                      : const TextStyle(color: AppColors.black),
+                ),
+              ),
+            );
+          },
+        ).toList(),
+      ),
     );
   }
 }
