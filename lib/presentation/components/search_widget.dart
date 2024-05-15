bb85638939384a8766d231d3d28d3f26b96884f5
nala0897@naver.com <nala0897@naver.com>
2024-05-15 17:15:00 +0900

    feat: 사용자가 선택한 음식을 db 에 저장

--- lib/presentation/components/search_widget.dart
+++ lib/presentation/components/search_widget.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:search_ex/data_source/db.dart';
 import 'package:search_ex/model/food.dart';
 
 class SearchWidget extends StatelessWidget {
@@ -11,13 +15,60 @@ class SearchWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(food.foodName),
-        Text('탄수화물 ${food.carbon}'),
-        Text('단백질 ${food.protein}'),
-        Text('지방 ${food.saturatedFat}'),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${food.foodName}(${food.makerName})',
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(
+                children: [
+                  Text('탄수화물 ${food.carbon}'),
+                  Text('단백질 ${food.protein}'),
+                  Text('지방 ${food.saturatedFat}'),
+                ],
+              ),
+            ],
+          ),
+          IconButton(
+            onPressed: () async {
+              final Food selectedFood = Food(
+                foodName: food.foodName,
+                makerName: food.makerName,
+                servingsize: food.servingsize,
+                servingunit: food.servingunit,
+                calories: food.calories,
+                carbon: food.carbon,
+                sugars: food.sugars,
+                protein: food.protein,
+                sodium: food.sodium,
+                saturatedFat: food.saturatedFat,
+                transFat: food.transFat,
+              );
+
+              String key = food.foodName;
+              String value = jsonEncode(selectedFood.toJson());
+              // DB 에 저장
+              final result = await prefs.setString(key, value);
+              if (result) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('저장되었습니다.')),
+                );
+              }
+            },
+            icon: const Icon(CupertinoIcons.plus_circle_fill),
+          ),
+        ],
+      ),
     );
   }
 }
