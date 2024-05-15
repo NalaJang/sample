70bee2d015c383cf9c906e279fea1dcb6e8ed1fe
nala0897@naver.com <nala0897@naver.com>
2024-05-15 23:10:47 +0900

    feat: appBar 에 검색 기능 구현

--- lib/presentation/search_screen.dart
+++ lib/presentation/search_screen.dart
@@ -3,42 +3,51 @@ import 'package:provider/provider.dart';
 import 'package:search_ex/presentation/components/search_widget.dart';
 import 'package:search_ex/presentation/viewModel/search_view_model.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final _searchTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<SearchViewModel>();
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: TextField(
+          controller: viewModel.queryTextEditingController,
+          decoration: InputDecoration(
+            hintText: '검색어를 입력해 주세요.',
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              final query = viewModel.queryTextEditingController.text;
+              viewModel.onSearchFood(query);
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.fromLTRB(10, 20, 10, 30),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: viewModel.queryTextEditingController,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                hintText: '검색어',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    final query = viewModel.queryTextEditingController.text;
-                    viewModel.onSearchFood(query);
-                  },
-                ),
-              ),
-            ),
             Expanded(
               child: ListView(
                 children: viewModel.filteredFoodList
-                    .map((food) => SearchWidget(
-                          food: food,
-                          preferences: viewModel.preferences,
-                        ))
+                    .map(
+                      (food) => SearchWidget(
+                        food: food,
+                        preferences: viewModel.preferences,
+                      ),
+                    )
                     .toList(),
               ),
             ),
