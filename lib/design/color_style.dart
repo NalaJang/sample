c6d199dbb4881c7f8d9a9dfaf63be752d87e55e2
nala0897@naver.com <nala0897@naver.com>
2024-05-15 22:09:12 +0900

    feat: 디자인 컬러 생성

---
+++ lib/design/color_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle {
+  static const Color purple = Color(0xffb8b8d1);
+  static const Color deepPurple = Color(0xff5b5f97);
+  static const Color yellow = Color(0xffffc145);
+  static const Color coral = Color(0xffff6b6c);
+  static const Color red = Color(0xffe5494f);
+  static const Color white = Color(0xfffffffb);
+  static const Color black = Color(0xff1e1e1e);
+}
