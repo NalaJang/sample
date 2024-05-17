faace2c9aa530eb5c0c084ffbabb36604374910d
nala0897@naver.com <nala0897@naver.com>
2024-05-17 11:47:19 +0900

    feat: gray color 스타일 추가

--- lib/design/color_style.dart
+++ lib/design/color_style.dart
@@ -5,7 +5,11 @@ class AppColors {
   static const Color purple2 = Color(0xff5b5f97);
   static const Color yellow = Color(0xffffc145);
   static const Color coral = Color(0xffff6b6c);
+  static const Color gray1 = Color(0xffb5b5b5);
+  static const Color gray2 = Color(0xff979797);
+  static const Color gray3 = Color(0xff545454);
   static const Color red = Color(0xffe5494f);
   static const Color white = Color(0xfffffffb);
   static const Color black = Color(0xff1e1e1e);
+  static const Color background = Color(0xfffdfdfd);
 }
